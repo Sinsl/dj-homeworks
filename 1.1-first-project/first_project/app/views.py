933a45c7d45309774baa4697a6ee6d3f0c01eb36
Sinsl <sinsl@me.com>
2024-04-13 22:47:14 +0300

    hw complite

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,17 +1,20 @@
-from django.http import HttpResponse
+import os
+from datetime import datetime
+
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, reverse
 
 
-def home_view(request):
+def home_view(request: HttpRequest) -> HttpResponse:
     template_name = 'app/home.html'
     # впишите правильные адреса страниц, используя
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -20,16 +23,23 @@ def home_view(request):
     return render(request, template_name, context)
 
 
-def time_view(request):
+def time_view(request: HttpRequest) -> HttpResponse:
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    now = datetime.now()
+    current_time = now.strftime("%d-%m-%Y %H:%M")
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
-def workdir_view(request):
+def workdir_view(request: HttpRequest) -> HttpResponse:
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    list_files = os.listdir(path='.')
+
+    if not list_files:
+        raise NotImplemented
+    str_files = '<br>'.join(list_files)
+    print(str_files)
+    return HttpResponse(str_files)
